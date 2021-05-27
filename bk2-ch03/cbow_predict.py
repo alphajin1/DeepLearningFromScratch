a24561109cdd03fd8c164adad2d66dfc51a38714
alphajin1 <alphajin1@gmail.com>
2021-05-27 21:05:29 +0900

    complete bk2-ch03 word2vec

---
+++ bk2-ch03/cbow_predict.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+"""
+cbow model에서 window=1로 설정한 모델
+"""
+import sys
+
+sys.path.append('..')
+import numpy as np
+from common.layers import MatMul
+
+# 샘플 맥락 데이터
+c0 = np.array([[1, 0, 0, 0, 0, 0, 0]])
+c1 = np.array([[0, 0, 1, 0, 0, 0, 0]])
+
+# 가중치 초기화
+W_in = np.random.randn(7, 3)
+W_out = np.random.randn(3, 7)
+
+# 계층 생성
+in_layer0 = MatMul(W_in)
+in_layer1 = MatMul(W_in) # 인코더
+out_layer = MatMul(W_out) # 디코더
+
+# 순전파
+h0 = in_layer0.forward(c0)
+h1 = in_layer1.forward(c1)
+h = 0.5 * (h0 + h1)
+s = out_layer.forward(h)
+print(s)
